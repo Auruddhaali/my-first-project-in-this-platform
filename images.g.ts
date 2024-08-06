b0e077ca0bfc2f6efbc8cc1c6c7f5cc50bc7e5ea
Auruddha Rushd Ali <92882285+Auruddhaali@users.noreply.github.com>
2024-08-06 20:38:04 +0600

    Update pxt.json, images.g.jres, images.g.ts

---
+++ images.g.ts
@@ -0,0 +1,27 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "song1":
+            case "mySong":return hex`0078000408020100001c00010a006400f4016400000400000000000000000000000000050000044e0000000400012408000c00012410001400012414001800012718001c00012a20002400012724002800012428002c000220292c003000021d2430003400042225272a3400380002242a38003c000127`;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
